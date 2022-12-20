a1f061da226260d8858679fae6b82646e63921ac
Mohamed Hemdan <hemdan@drjobs.ae>
2022-12-20 08:23:41 +0200

    assets updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './shared/fonts/stylesheet.css';
 import './shared/index.css';
 import App from './App';
 
